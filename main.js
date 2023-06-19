c22197f93b46858b19d663944a62c7fe090e57bd
Abdul Majid Irfan <78084618+Irfan-Majid@users.noreply.github.com>
2023-06-19 22:13:29 +0600

    done

--- main.js
+++ main.js
@@ -4,7 +4,12 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            sizes: [
+                {width:10, height:20},
+                {width:15, height:25},
+                {width:20, height:30}
+            ]
         }
     }
 })
