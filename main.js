4c904987a9c08b19d23edba9945edb0e3775137a
Abdul Majid Irfan <78084618+Irfan-Majid@users.noreply.github.com>
2023-06-20 06:29:43 +0600

    done

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             product: 'Socks',
             brand: 'Vue Mastery',
             image: './assets/images/socks_blue.jpg',
-            inStock: false,
+            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
               { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
@@ -20,5 +20,14 @@ const app = Vue.createApp({
         updateImage(variantImage) {
             this.image = variantImage
         }
+    },
+    computed:{
+        forSale(){
+            if(this.onSale){
+                return this.product + ' ' + this.brand + ' ' + 'is for sale';
+            }
+            return '';
+        }
+
     }
 })
